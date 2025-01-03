857d3ba1351802e6360e7e60a1249347214aa561
Raiane Batista <153535785+RaianeBatista@users.noreply.github.com>
2025-01-03 14:29:01 -0300

    feat:  add converte ts em js

--- script.ts
+++ script.ts
@@ -1,17 +1,15 @@
 // tsc --version
-let numero1 = document.getElementById('numero1');
-let numero2 = document.getElementById('numero2');
+let numero1 = document.getElementById('numero1') as HTMLInputElement;
+let numero2 = document.getElementById('numero2') as HTMLInputElement;
 let botao = document.getElementById('calcular');
 let res = document.getElementById('resultado');
 
 function calcular(n1, n2) {
-  if (typeof n1 == 'number' && typeof n2 == 'number') {
-    return n1 + n2;
-  } else {
     return +n1 + +n2;
   }
-}
 
 botao.addEventListener('click', function () {
-  res.innerHTML = calcular(numero1.value, numero2.value);
+  if (res) {
+    res.innerHTML = calcular(numero1.value, numero2.value);
+  }
 });
