9610c863345e55817b80f18c018957a7539673c8
Raiane Batista <153535785+RaianeBatista@users.noreply.github.com>
2025-01-04 11:59:01 -0300

    feat: add nome : string

--- src/script.ts
+++ src/script.ts
@@ -1,8 +1,7 @@
 //Usando types no retorno de uma função
-function firstLetterUppercase(name: string): number {
+function firstLetterUppercase(name: string): string {
   let firstLetter = name.charAt(0).toUpperCase();
-  return 16;
- // return firstLetter + name.substring(1);
+ return firstLetter + name.substring(1);
 }
 
-let nome: number = firstLetterUppercase('raiane');
+let nome: string = firstLetterUppercase('raiane');
