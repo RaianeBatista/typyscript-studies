af96f29ab257fc1b8b22e5f580fa139287915d7c
Raiane Batista <153535785+RaianeBatista@users.noreply.github.com>
2025-01-04 11:58:04 -0300

    feat: add return number

--- src/script.ts
+++ src/script.ts
@@ -1,7 +1,8 @@
 //Usando types no retorno de uma função
 function firstLetterUppercase(name: string): number {
   let firstLetter = name.charAt(0).toUpperCase();
-  return firstLetter + name.substring(1);
+  return 16;
+ // return firstLetter + name.substring(1);
 }
 
 let nome: number = firstLetterUppercase('raiane');
