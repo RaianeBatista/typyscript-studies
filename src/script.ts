d43cbcbfc067a7fd6a128a969d5eecf9de0cf438
Raiane Batista <153535785+RaianeBatista@users.noreply.github.com>
2025-01-04 12:04:44 -0300

    feat: substituindo por um template string

--- src/script.ts
+++ src/script.ts
@@ -1,7 +1,7 @@
 //Usando types no retorno de uma função
 //Deve-se typar o retorno e a entrada no paramentro da função
-function somar(n1: number, n2: number):number {
-  return n1 + n2;
+function somar(n1: number, n2: number) {
+  return `${n1 + n2}`;
 }
 
 let alguma = somar(12, 17)
