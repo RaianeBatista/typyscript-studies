d88eeb2e01d1c1ad0fbb30e0b00485a5c98a27fa
Raiane Batista <153535785+RaianeBatista@users.noreply.github.com>
2025-01-04 12:02:49 -0300

    feat: add type number

--- src/script.ts
+++ src/script.ts
@@ -1,5 +1,6 @@
 //Usando types no retorno de uma função
-function somar(n1, n2):number {
+//Deve-se typar o retorno e a entrada no paramentro da função
+function somar(n1: number, n2: number):number {
   return n1 + n2;
 }
 
