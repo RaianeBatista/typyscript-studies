9a7075fb60e9aac9c143ea00ef2d4563bf626598
Raiane Batista <153535785+RaianeBatista@users.noreply.github.com>
2025-01-04 23:09:17 -0300

    feat: type literal

--- src/script.ts
+++ src/script.ts
@@ -1,7 +1,6 @@
-type VerdadeiroOuFalso = true | false
+type VerdadeiroOuFalso = true | false;
 
-
-function temNome(nome: string): boolean {
+function temNome(nome: string): VerdadeiroOuFalso {
   if (nome !== '') {
     return true;
   } else {
