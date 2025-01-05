419d320f08e2891b63d46b7b782503ba004dc85c
Raiane Batista <153535785+RaianeBatista@users.noreply.github.com>
2025-01-04 21:44:30 -0300

    feta: add interface

--- src/script.ts
+++ src/script.ts
@@ -1,9 +1,13 @@
 //Type e Interface: Como usar e diferenças
-type User = {
-  nome: string,
-  idade: number
-}
+// type User = {
+//   nome: string,
+//   idade: number
+// }
 
+interface User {
+  nome: string;
+  idade: number;
+}
 
 function resumo(usuario: User) {
   return `Olá ${usuario.nome}, você tem ${usuario.idade} anos`;
