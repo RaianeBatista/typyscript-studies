341bd2382f8f50cc8805ff45d6a9b69223c69106
Raiane Batista <153535785+RaianeBatista@users.noreply.github.com>
2025-01-04 21:42:45 -0300

    feat: add type User

--- src/script.ts
+++ src/script.ts
@@ -5,7 +5,7 @@ type User = {
 }
 
 
-function resumo(usuario: { nome: string;  idade:number}) {
+function resumo(usuario: User) {
   return `Olá ${usuario.nome}, você tem ${usuario.idade} anos`;
 }
 
